dc92163867cc6daae4c6b75a2629110fe78e909a
Sam R Doubet <doubetsr@unk.edu>
2018-03-08 20:20:43 -0600

    install htop fix errors

--- 1.0.0/install.sh
+++ 1.0.0/install.sh
@@ -1,11 +1,10 @@
-# Tell it the shell interpreter also shebang is pound with explanation mark like the line below
 #!/usr/bin/with-contenv /bin/bash
 
 # Default runtime variable if none is supplied
 HELLO_WORLD_STATEMENT=${HELLO_WORLD_STATEMENT:='You are lazy since you are using the default value and did not specify this variable.'}
 
-ECH "You said: "
+echo "You said: "
 echo $HELLO_WORLD_STATEMENT
 
-# Self Destruct to keep from duplication or overwriting and it removes itself
+# Self Destruct
 rm -- "$0" && exit 0
